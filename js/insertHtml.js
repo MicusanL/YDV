7e3e754553cb4636612c0cd7e41a372856c328c3
Lidia Micusan <lidia.micusan@visma.com>
2025-09-05 16:05:29 +0300

    Refactor insertHtml.js to ensure Webflow initialization after loading sections

--- js/insertHtml.js
+++ js/insertHtml.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
-  $("#navbar-section").load("/html-resources/menu.html");
-  $("#contact-section").load("/html-resources/contact-section.html");
+  $("#navbar-section").load("/html-resources/menu.html", function() {
+    if (window.Webflow && Webflow.require) {
+      Webflow.require('ix2').init();
+    }
+  });
+  $("#contact-section").load("/html-resources/contact-section.html", function() {
+    if (window.Webflow && Webflow.require) {
+      Webflow.require('ix2').init();
+    }
+  });
   selectCurrentPageMenu();
 });
 
 function selectCurrentPageMenu () {
   if ($('.pages-box').is(':visible')) {
-	let file = $(document)[0].URL.split('/').pop();
-	let tab = $('.pages-box a[href="' + file + '"]').first();
+    let file = $(document)[0].URL.split('/').pop();
+    let tab = $('.pages-box a[href="' + file + '"]').first();
     if (tab.length === 1){
-		$(tab).addClass('w--current');
-	} else {
-		$('.pages-box a[href="index.html"]').first().addClass('w--current');
-	}
+        $(tab).addClass('w--current');
+    } else {
+        $('.pages-box a[href="index.html"]').first().addClass('w--current');
+    }
   } else {
     setTimeout(selectCurrentPageMenu, 50); //wait 50 ms, then try again
   }
