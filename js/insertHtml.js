d96bf160ef4612f5d2778c515ce95614a6104ed9
Lidia Micusan <lidia.micusan@visma.com>
2025-09-05 17:48:54 +0300

    yearSpan

--- js/insertHtml.js
+++ js/insertHtml.js
@@ -1,29 +1,33 @@
-$(document).ready(function() {
-  $("#navbar-section").load("/html-resources/menu.html", function() {
+$(document).ready(function () {
+  $("#navbar-section").load("/html-resources/menu.html", function () {
     if (window.Webflow && Webflow.require) {
       var ix2 = Webflow.require('ix2');
       ix2.destroy();
       ix2.init();
     }
   });
-  $("#contact-section").load("/html-resources/contact-section.html", function() {
+  $("#contact-section").load("/html-resources/contact-section.html", function () {
     if (window.Webflow && Webflow.require) {
       var ix2 = Webflow.require('ix2');
       ix2.destroy();
       ix2.init();
+    }
+    var yearSpan = document.getElementById('current-year');
+    if (yearSpan) {
+      yearSpan.textContent = new Date().getFullYear();
     }
   });
   selectCurrentPageMenu();
 });
 
-function selectCurrentPageMenu () {
+function selectCurrentPageMenu() {
   if ($('.pages-box').is(':visible')) {
     let file = $(document)[0].URL.split('/').pop();
     let tab = $('.pages-box a[href="' + file + '"]').first();
-    if (tab.length === 1){
-        $(tab).addClass('w--current');
+    if (tab.length === 1) {
+      $(tab).addClass('w--current');
     } else {
-        $('.pages-box a[href="index.html"]').first().addClass('w--current');
+      $('.pages-box a[href="index.html"]').first().addClass('w--current');
     }
   } else {
     setTimeout(selectCurrentPageMenu, 50); //wait 50 ms, then try again
